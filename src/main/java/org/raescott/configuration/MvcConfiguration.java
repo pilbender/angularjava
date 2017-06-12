1fef5748e63088c7a03490684d3060032afab56b
Richard Scott Smith <scott.smith@isostech.com>
2017-06-12 14:23:43 -0700

    Working prototype of static html and dynamic JSPs being served up.  A little more tweaking to come.

--- src/main/java/org/raescott/configuration/MvcConfiguration.java
+++ src/main/java/org/raescott/configuration/MvcConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.web.accept.ContentNegotiationManagerFactoryBean;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -23,7 +25,12 @@ import static org.springframework.http.MediaType.TEXT_HTML_VALUE;
  */
 @Configuration
 @EnableWebMvc
-public class MvcConfiguration extends WebMvcConfigurerAdapter {
+public class MvcConfiguration extends WebMvcConfigurerAdapter /*WebMvcConfigurationSupport*/ {
+	// Taken from Spring Boot code itself
+	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+			"classpath:/META-INF/resources/", "classpath:/resources/",
+			"classpath:/static/", "classpath:/public/" };
+
 	@Bean
 	public ContentNegotiationManagerFactoryBean contentNegotiationManagerFactoryBean() {
 		ContentNegotiationManagerFactoryBean contentNegotiationManagerFactoryBean = new ContentNegotiationManagerFactoryBean();
@@ -66,4 +73,44 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter {
 		return contentNegotiatingViewResolver;
 	}
 
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		if (!registry.hasMappingForPattern("/webjars/**")) {
+			registry.addResourceHandler("/webjars/**").addResourceLocations(
+					"classpath:/META-INF/resources/webjars/");
+		}
+		if (!registry.hasMappingForPattern("/**")) {
+			registry.addResourceHandler("/**").addResourceLocations(
+					CLASSPATH_RESOURCE_LOCATIONS);
+		}
+	}
+
+	/**
+	 * equivalent for the <mvc:resources mapping="/resources/**" location="/resources/" /> tag
+	 * @param registry
+	 */
+	// no work!
+	/*
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+	*/
+
+	/*
+	@Override
+	@Bean
+	public HandlerMapping resourceHandlerMapping() {
+		AbstractHandlerMapping handlerMapping = (AbstractHandlerMapping) super.resourceHandlerMapping();
+		handlerMapping.setOrder(-1);
+		return handlerMapping;
+	}
+
+	// equivalent for the <mvc:default-servlet-handler/> tag
+	// no work!
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+	*/
 }
